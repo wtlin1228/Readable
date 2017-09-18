cb7c952f22a8c839673d16fdf37ad8ad360fa823
wtlin1228 <wtlin0711@gmail.com>
2017-09-18 16:09:01 +0800

    Remove saga: navigation

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,12 +2,10 @@ import { fork, all } from 'redux-saga/effects'
 
 import { watchGetCategory } from "./category";
 import { watchGetAllPosts } from "./post";
-import { watchNavigateCategory } from "./navigation";
 
 export default function* rootSaga() {
   yield all([
     fork(watchGetCategory),
     fork(watchGetAllPosts),
-    fork(watchNavigateCategory)
   ])
 }
--- src/sagas/navigation.js
+++
@@ -1,8 +0,0 @@
-import { takeLatest } from 'redux-saga/effects'
-
-import * as types from '../constants/actionTypes'
-import { worker as get_post_worker } from './post'
-
-export function* watchNavigateCategory() {
-  yield takeLatest(types.NAVIGATE_CATEGORY, get_post_worker);
-}
